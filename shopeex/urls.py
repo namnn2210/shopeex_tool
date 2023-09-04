4b4237a38b2b0853d7e7bc4fa9e10e78a4f38f74
namng <nam.nn@bidgear.com>
2023-09-04 22:33:01 +0700

    edit

--- shopeex/urls.py
+++ shopeex/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", view=views.index, name="index"),
     path("rating", view=views.rating_order, name="rating"),
-    path("accounts/login/", view=views.login, name="login"),
+    path("accounts/login/", view=views.login_view, name="login"),
+    path("accounts/logout/", view=views.logout_view, name="logout"),
+    path("get_info", view=views.get_info, name="get_info"), 
     path("register", view=views.register, name="register"),
     path("forgot", view=views.forgot, name="forgot")
 ]
--- shopeex/views.py
+++ shopeex/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
 from loguru import logger
+
+import requests
 
 @login_required
 def index(request):
@@ -12,16 +14,29 @@ def index(request):
 def rating_order(request):
     return render(request, 'rating.html')
 
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index') 
+        else:
+            error_message = "Invalid credentials. Please try again."
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
 
 def forgot(request):
     return render(request, 'forgot.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-        logger.info('========================== %s' % form)
         if form.is_valid():
             user = form.save(commit=False)
             user.first_name = form.cleaned_data['first_name']
@@ -33,3 +48,12 @@ def register(request):
     else:
         form = RegistrationForm()
     return render(request, 'register.html', {'form': form})
+
+def get_info(request):
+    if request.method == 'POST':
+        params = {
+            'cookie' : request.POST['cookie']
+        }
+        user_info = requests.post("http://localhost:2210/get_account_info", params=params).json()['data']['user_profile']
+        logger.info(user_info)
+        return render(request, 'rating.html', {'user_info':user_info})

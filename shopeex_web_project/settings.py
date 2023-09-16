fd5cd8302a60d3de58b3db501326b1cc29f95bc3
namng <nam.nn@bidgear.com>
2023-09-16 13:41:13 +0700

    edit

--- shopeex_web_project/settings.py
+++ shopeex_web_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'shopeex_web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NamNN',
+        'USER': 'root',
+        'PASSWORD': '!@Ngocnam2210',
+        'HOST': 'localhost',  # or the hostname of your MySQL server
+        'PORT': '3306',  # MySQL default port is 3306
     }
 }
 
